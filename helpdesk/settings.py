ccf1871e894d811b90a9e37fd2e0011ca45c97dd
Nikitadevp <jrdme.rbpl@rajat-group.com>
2026-02-12 15:42:55 +0530

    Fixed SECRET_KEY configuration

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-0(s13&9xz$!mt9+^84%)&ymc-w_(m*6y-6#k=0*jpxukk$tz8-')
+ #SECRET_KEY = os.environ.get('django-insecure-0(s13&9xz$!mt9+^84%)&ymc-w_(m*6y-6#k=0*jpxukk$tz8-')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 #SECRET_KEY = 'django-insecure-0(s13&9xz$!mt9+^84%)&ymc-w_(m*6y-6#k=0*jpxukk$tz8-'
 
 
