7ada72bb7906cb65c9cc14a3c77d43e606703e79
Nikitadevp <jrdme.rbpl@rajat-group.com>
2026-02-14 17:23:25 +0530

    Fixed root URL 500 error

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,21 +4,22 @@ from .views import raise_ticket, resolve_ticket,dashboard
 
 
 
+# urlpatterns = [
+#     path('raise-ticket/', raise_ticket, name='raise_ticket'),
+#     path('resolve-ticket/<str:ticket_no>/', resolve_ticket, name='resolve_ticket'),
+#     #path('', raise_ticket),
+#     #path('raise-ticket/', raise_ticket),
+
+#     #  Resolve Ticket Page
+#     #path("resolve-ticket/<str:ticket_no>/", resolve_ticket),
+#    #path("", dashboard, name="dashboard"),
+#     path("dashboard/", dashboard, name="dashboard"),   
+    
+#]
+
 urlpatterns = [
+    path('', raise_ticket, name='home'), 
     path('raise-ticket/', raise_ticket, name='raise_ticket'),
     path('resolve-ticket/<str:ticket_no>/', resolve_ticket, name='resolve_ticket'),
-    #path('', raise_ticket),
-    #path('raise-ticket/', raise_ticket),
-
-    #  Resolve Ticket Page
-    #path("resolve-ticket/<str:ticket_no>/", resolve_ticket),
-   #path("", dashboard, name="dashboard"),
     path("dashboard/", dashboard, name="dashboard"),
-
-  
-    
-
-   
-    
-    
 ]
