ead4bc828ff82545eff6b59a82d103b3ad0453f2
Nikitadevp <jrdme.rbpl@rajat-group.com>
2026-02-14 17:27:51 +0530

    Fixed template DIRS issue

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -66,10 +66,28 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'helpdesk.urls'
 
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],   # 👈 empty 
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],   # 👈 empty 
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
