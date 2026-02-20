38755e50282bb26af8ed3d2271aa526b806d03bd
Nikitadevp <jrdme.rbpl@rajat-group.com>
2026-02-20 11:30:02 +0530

    Fix settings

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -178,9 +178,12 @@ USE_TZ = True
 #     BASE_DIR / 'tickets' / 'static',
 # ]
 
+STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 
